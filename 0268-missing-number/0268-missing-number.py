6fa55e2569a301ff26ae55f8208271501c7a71eb
Sushma Niresh <49243853+Sushmaniresh@users.noreply.github.com>
2026-06-17 17:19:45 -0700

    Time: 0 ms (100%), Space: 20.5 MB (19.44%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        for i in range(1,n+1):
-            if i not in nums:
-                return i   
-        return 0    
+        expected = n * (n + 1) // 2
+        actual = sum(nums)
+        return expected - actual   
