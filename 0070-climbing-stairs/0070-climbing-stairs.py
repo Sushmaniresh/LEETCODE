863a22bea456e3c946cedc524a4793966b371516
Sushma Niresh <49243853+Sushmaniresh@users.noreply.github.com>
2026-09-04 22:22:38 -0400

    Time: 0 ms (100%), Space: 19.3 MB (53.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one , two = 1, 1 
+        one, two = 1,1
         for i in range(n-1):
-            one, two = two, one+two
-        return two
-
-        
-
-
-
+            one,two = one+two,one
+        return one
         
